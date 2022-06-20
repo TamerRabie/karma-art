ada2f578172ac240f84e3352e8e68baf731a3af5
TamerRabie <tamerrabie2015@gmail.com>
2022-06-20 15:33:16 -0400

    Admin Progress

--- src/karma-admin/links/notifications-link/notifications-link.component.ts
+++ src/karma-admin/links/notifications-link/notifications-link.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AdminService} from "../../services/AdminService";
 
 @Component({
   selector: 'notifications-link',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NotificationsLinkComponent implements OnInit {
 
-  constructor() {
+  constructor(public adminService: AdminService) {
   }
 
   ngOnInit(): void {
--- src/karma-admin/links/users-link/users-link.component.ts
+++ src/karma-admin/links/users-link/users-link.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AdminService} from "../../services/AdminService";
 
 @Component({
   selector: 'users-link',
@@ -7,7 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UsersLinkComponent implements OnInit {
 
-  constructor() {
+  constructor(public adminService: AdminService) {
   }
 
   ngOnInit(): void {
